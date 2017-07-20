43093097c63ada747920610754cd3c6c596624b2
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-20 09:38:17 +0700

    Done testcase 05

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,6 +1,7 @@
 public class FizzBuzz {
     public String say(int input) {
         if (input == 3) return "Fizz";
+        if (input == 5) return "Buzz";
         return Integer.toString(input);
     }
 }
