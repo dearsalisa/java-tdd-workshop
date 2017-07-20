7c9de24862e45302617b90ad63bdc44696a52c0f
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-20 16:46:07 +0700

    [Prime Factor] fix code

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -4,13 +4,15 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List result = new ArrayList();
-        while (number%2 == 0) {
-            result.add(2);
-            number /= 2;
-        }
-        while (number%3 == 0) {
-            result.add(3);
-            number /= 3;
+        while (number > 1) {
+            if(number%2 == 0){
+                result.add(2);
+                number /= 2;
+            }
+            if(number%3 == 0) {
+                result.add(3);
+                number /= 3;
+            }
         }
         if(number > 1) result.add(number);
         return result;
