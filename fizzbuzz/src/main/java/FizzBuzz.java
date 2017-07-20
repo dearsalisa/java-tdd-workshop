6b8d209ae3900379ecd2257c94f721e798b4c7a8
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-20 09:47:34 +0700

    Done FizzBuzz

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public String say(int input) {
-        if (input == 15) return "FizzBuzz";
+        if (input%15 == 0) return "FizzBuzz";
         else if (input%5 == 0) return "Buzz";
         else if (input%3 == 0) return "Fizz";
         return Integer.toString(input);
