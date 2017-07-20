b273756729fa3c9a1e2329e93f727306661f1f56
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-20 16:31:39 +0700

    [Prime Factor] Done testcase 04 & 05

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -4,6 +4,14 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List result = new ArrayList();
+        if(number%2 == 0) {
+            result.add(2);
+            number /= 2;
+        }
+        if(number%3 == 0) {
+            result.add(3);
+            number /= 3;
+        }
         if(number > 1) result.add(number);
         return result;
     }
