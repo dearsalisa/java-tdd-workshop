60fb4377ecc6ac47409b84a0f506c18cbd46f1e5
KBANKNET\salisa.at <salisa.at@kbtg.tech>
2017-07-20 15:12:57 +0700

    [Format Phone Number] Done testcase 04

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -7,7 +7,14 @@ public class FormatPhoneNumber {
     }
 
     public String solution(String input) {
-        return null;
+        String result = "";
+        for (int i = 1; i <= input.length(); i++) {
+            result += input.charAt(i-1);
+            if (i%3 == 0 && i != input.length()) {
+                result += "-";
+            }
+        }
+        return result;
     }
 
 }
